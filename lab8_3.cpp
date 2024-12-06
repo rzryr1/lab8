df2ec0943be1829caa0f0a5e879ba1945eb10f9a
rzryr1 <solotogre@gmail.com>
2024-12-06 16:18:59 +0700

    lab8_3.cpp completed

--- lab8_3.cpp
+++ lab8_3.cpp
@@ -1,3 +1,50 @@
+#include <iostream>
+using namespace std;
+
+int age;
+int height;
+long long bounty;
+int main(){
+    cout << "Enter your age: ";
+    cin >> age; 
+    if(age <= 25){
+        cout << "Enter your height: ";
+        cin >> height;       
+        if(height < 100){
+            cout << "Your character = Chopper";
+        }else{
+            if(height < 180 && height >= 100){
+            cout << "Your character = Usopp";
+        }else{
+            cout << "Enter your bounty: ";
+            cin >> bounty;
+            if(bounty > 1100000000){
+                cout << "Your character = Zoro";
+            }else{
+                cout << "Your character = Sanji";
+            }
+        }
+        }
+    }else{
+        if(age <=60){
+            cout << "Enter your bounty: ";
+            cin >> bounty;
+            if(bounty > 500000000){
+                cout << "Your character = Jinbe";
+            }else{
+                cout << "Your character = Franky";
+            }
+        }else{
+            cout << "Your character = Brook";
+        }
+        
+    }
+
+}
+
+
+
+
 /*
 "Enter your age: "
 "Enter your height: "
